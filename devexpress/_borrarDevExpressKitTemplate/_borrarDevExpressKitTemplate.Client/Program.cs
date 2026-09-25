6ebd7365f17451e4f565e504f5c70b4e651668a9
Omar I. Martínez Gómez <omar_238@hotmail.com>
2026-09-25 09:02:48 -0600

    devexpress -> corrección de los proyectos de prueba de mvc

---
+++ devexpress/_borrarDevExpressKitTemplate/_borrarDevExpressKitTemplate.Client/Program.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddDevExpressBlazor(options => {
+    options.SizeMode = DevExpress.Blazor.SizeMode.Medium;
+});
+
+await builder.Build().RunAsync();
---
+++ devexpress/_borrarDevExpressKitTemplate/_borrarDevExpressKitTemplate/Program.cs
@@ -0,0 +1,39 @@
+﻿using _borrarDevExpressKitTemplate.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddDevExpressBlazor(options => {
+    options.SizeMode = DevExpress.Blazor.SizeMode.Medium;
+});
+
+builder.Services.AddMvc();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if(app.Environment.IsDevelopment()) {
+    app.UseWebAssemblyDebugging();
+}
+else {
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(_borrarDevExpressKitTemplate.Client._Imports).Assembly)
+    .AllowAnonymous();
+
+app.Run();
