bb59037ba2068eb72371c9180649da0e7446931c
Omar I. Martínez Gómez <omar_238@hotmail.com>
2026-09-25 23:30:43 -0600

    efcore10 -> pp 31(50), (fin de captura del modelo del bloque de codigo)

---
+++ efcore10/EF_ConsoleProgram/EF_DBLibrary/EFDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer.Infrastructure.Internal;
+using Microsoft.Extensions.Configuration;
+
+namespace EF_DBLibrary;
+
+public class EFDbContext : DbContext
+{
+    public EFDbContext() { }
+    public EFDbContext(DbContextOptions<EFDbContext> options) : base(options) { }
+
+    override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.Options.Extensions.OfType<SqlServerOptionsExtension>().Any())
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var connStr = config.GetConnectionString("EFDbConnection");
+            optionsBuilder.UseSqlServer(connStr);
+        }
+    }
+}
